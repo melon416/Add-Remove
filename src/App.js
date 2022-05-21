23f6ea43a667dadb3ed503fed9647c95ddfcee81
melon416 <elitedev416@gmail.com>
2022-05-21 10:04:22 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -2,26 +2,36 @@ import React, { useState } from 'react';
 import './App.css';
 
 const MainContainer = () => {
-  const [number, setNumber] = useState(0);
+  const [data, setData] = useState([]);
 
   const handleAdd = () => {
-    setNumber(number + 1)
+    var temp = JSON.parse(JSON.stringify(data));
+    temp.push(data.length + 1);
+    setData(temp);
   };
 
   const handleRemove = () => {
-    setNumber(number - 1)
+    if(data.length > 1) {
+      var temp = JSON.parse(JSON.stringify(data));
+      temp.splice(data.length - 1, 1);
+      setData(temp);
+    }
   }
 
   return (
-    <>
-      <div>
+    <div className="wrapper">
+      <div className='wrapper-button'>
         <button onClick={handleAdd}>Add</button>
         <button onClick={handleRemove}>Remove</button>
       </div>
-      <div>
-        {number}
+      <div className='wrapper-body'>
+        {data.map((item) => (
+          <div key={item} className="item">
+            {item}
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   )
 
 }
